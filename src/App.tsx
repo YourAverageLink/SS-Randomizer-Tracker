d2c7e37227125e72285b7bed6e2aa6135415568b
robojumper <robojumper@gmail.com>
2025-01-04 14:26:16 +0100

    Optimize color scheme to not require rerendering the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,38 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FullAcknowledgement from './miscPages/FullAcknowledgement';
 import Options from './options/Options';
 import Tracker from './Tracker';
-import { useSelector } from 'react-redux';
-import { colorSchemeSelector } from './customization/Selectors';
-import { useLayoutEffect } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorPage from './miscPages/ErrorPage';
+import { useStore } from 'react-redux';
+import type { RootState } from './store/Store';
+import { useLayoutEffect } from 'react';
+import type { ColorScheme } from './customization/ColorScheme';
+import { colorSchemeSelector } from './customization/Selectors';
 
-function App() {
-    const colorScheme = useSelector(colorSchemeSelector);
-    useLayoutEffect(() => {
+function createApplyColorSchemeListener() {
+    let prevScheme: ColorScheme | undefined = undefined;
+    return (colorScheme: ColorScheme) => {
+        if (colorScheme === prevScheme) {
+            return;
+        }
+        console.log('updating scheme')
+        prevScheme = colorScheme;
         const html = document.querySelector('html')!;
         Object.entries(colorScheme).forEach(([key, val]) => {
             html.style.setProperty(`--scheme-${key}`, val.toString());
         });
-    }, [colorScheme]);
+    };
+}
+
+function App() {
+    const store = useStore<RootState>();
+    useLayoutEffect(() => {
+        const listener = createApplyColorSchemeListener();
+        listener(colorSchemeSelector(store.getState()));
+        return store.subscribe(() =>
+            listener(colorSchemeSelector(store.getState())),
+        );
+    }, [store]);
 
     return (
         <ErrorBoundary FallbackComponent={ErrorPage}>
